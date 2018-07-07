642d4e7a06785768d29488cca267326fa6d35e27
edjw <edjw@users.noreply.github.com>
2018-07-07 19:05:53 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-window.onscroll = function () {
-    moveScrollIndicator()
-};
+if (document.getElementById("scroll_indicator")) {
+    window.onscroll = function () {
+        moveScrollIndicator()
+    };
 
-function moveScrollIndicator() {
-    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    var scrolled = (winScroll / height) * 100;
-    if (document.getElementById("scroll_indicator")) {
+    function moveScrollIndicator() {
+        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        var scrolled = (winScroll / height) * 100;
         document.getElementById("scroll_indicator").style.width = scrolled + "%";
     }
 }
