08795dc2cf72a2a74bf6d97eb8453b2e860f9455
edjw <edjw@users.noreply.github.com>
2018-07-07 19:02:57 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,5 +6,7 @@ function moveScrollIndicator() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
     var scrolled = (winScroll / height) * 100;
-    document.getElementById("scroll_indicator").style.width = scrolled + "%";
+    if (document.getElementById("scroll_indicator")) {
+        document.getElementById("scroll_indicator").style.width = scrolled + "%";
+    }
 }
