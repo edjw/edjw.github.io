72e28f83a357ceed01ab390a1a3766232742eaac
Personal <personal@Eds-MacBook-Pro.local>
2018-07-07 11:28:36 +0100

    adding scoll indicator

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.onscroll = function () {
+    moveScrollIndicator()
+};
+
+function moveScrollIndicator() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("scroll_indicator").style.width = scrolled + "%";
+}
